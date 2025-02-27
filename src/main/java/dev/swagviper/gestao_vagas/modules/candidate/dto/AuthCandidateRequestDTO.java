4d6ace87e1670504338d6080b505ad2d28924b6d
swagviper <bieelgod6212@gmail.com>
2025-02-27 13:06:10 -0300

    feat: added candidate token authorization

---
+++ src/main/java/dev/swagviper/gestao_vagas/modules/candidate/dto/AuthCandidateRequestDTO.java
@@ -0,0 +1,4 @@
+package dev.swagviper.gestao_vagas.modules.candidate.dto;
+
+public record AuthCandidateRequestDTO(String username, String password) {
+}
---
+++ src/main/java/dev/swagviper/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,57 @@
+package dev.swagviper.gestao_vagas.modules.candidate.useCases;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import dev.swagviper.gestao_vagas.modules.candidate.CandidateRepository;
+import dev.swagviper.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import dev.swagviper.gestao_vagas.modules.candidate.dto.AuthCandidateResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.naming.AuthenticationException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+@Service
+public class AuthCandidateUseCase {
+
+    @Value("${security.token.secret.candidate}")
+    private String secretKey;
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO) throws AuthenticationException {
+        var candidate = this.candidateRepository.findByUsername(authCandidateRequestDTO.username())
+            .orElseThrow(() -> new UsernameNotFoundException("Usuário ou senha incorretas."));
+
+        var passwordMatches = this.passwordEncoder.matches(authCandidateRequestDTO.password(), candidate.getPassword());
+
+        if (!passwordMatches) {
+            throw new AuthenticationException();
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var expiresIn = Instant.now().plus(Duration.ofHours(2));
+        var token = JWT.create()
+            .withIssuer("swagviper")
+            .withSubject(candidate.getId().toString())
+            .withClaim("roles", Arrays.asList("candidate"))
+            .withExpiresAt(expiresIn)
+            .sign(algorithm);
+
+        var authCandidateResponse = AuthCandidateResponseDTO.builder()
+            .access_token(token)
+            .expires_in(expiresIn.toEpochMilli())
+            .build();
+
+        return authCandidateResponse;
+    }
+}
