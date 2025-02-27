a5cd0081e90d41b07f1883f3e9e7135dc5e209f4
swagviper <bieelgod6212@gmail.com>
2025-02-26 23:28:00 -0300

    feat: added jwt provider to token authorization

---
+++ src/main/java/dev/swagviper/gestao_vagas/providers/JWTProvider.java
@@ -0,0 +1,32 @@
+package dev.swagviper.gestao_vagas.providers;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTProvider {
+
+    @Value("${security.token.secret}")
+    private String secretKey;
+
+    public String validateToken(String token) {
+        token = token.replace("Bearer ", "").trim();
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        try {
+            var subject = JWT.require(algorithm)
+                .build()
+                .verify(token)
+                .getSubject();
+            return subject;
+        } catch (JWTVerificationException exception) {
+            exception.printStackTrace();
+            return "";
+        }
+    }
+
+
+}
